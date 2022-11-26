0b1b3e80eda20a59908a288eb4117a35d2004997
Redamancy9189 <1791081494@qq.com>
2022-11-26 23:08:28 +0800

    Add: Search Box

--- src/components/NavigationBar/index.vue
+++ src/components/NavigationBar/index.vue
@@ -4,17 +4,15 @@
  * @Author: 王远昭
  * @Date: 2022-11-01 18:10:26
  * @LastEditors: 王远昭
- * @LastEditTime: 2022-11-22 20:49:05
+ * @LastEditTime: 2022-11-24 17:09:50
 -->
 <!-- 导航栏组件 -->
 <script setup lang="ts">
-import { NIcon } from "naive-ui";
-import { SearchOutline } from "@vicons/ionicons5";
 import goto from "../../utils/goto";
 import SideBar from "../SideBar/index.vue";
+import SearchBox from "../SearchBox/index.vue";
 import logo from "../../assets/logo.png";
 let baseLogoUrl = logo;
-let size = { smallsize: 28, mediumsize: 32, largesize: 36 };
 </script>
 <template>
   <div class="nav-container">
@@ -28,10 +26,9 @@ let size = { smallsize: 28, mediumsize: 32, largesize: 36 };
       <img :src="baseLogoUrl" alt="Logo" />
     </div>
     <!-- 博客个人信息 -->
-    <div class="nav-right">
-      <n-icon :size="size.smallsize">
-        <SearchOutline />
-      </n-icon>
+    <div class="nav-right ripple">
+      <!-- 搜索栏 -->
+      <SearchBox/>
     </div>
   </div>
 </template>
@@ -39,27 +36,32 @@ let size = { smallsize: 28, mediumsize: 32, largesize: 36 };
 * {
   max-width: 100vw;
   display: inline-block;
+
   .nav-container {
     display: flex;
     justify-content: center;
     width: 100vw;
+
     .nav-left {
       width: 4rem;
     }
+
     .nav-middle {
       text-align: center;
-      width: calc(100% - 4rem - 5px); /* 目前的办法就是这个了 */
+      width: calc(100% - 4rem - 5px);
+
+      /* 目前的办法就是这个了 */
       img {
         user-select: none;
         max-width: 2rem;
       }
     }
+
     .nav-right {
       box-sizing: border-box;
-      width: 2rem;
-      float: right;
-      margin-right: 5px;
-      padding-top: 4px;
+      display: flex;
+      justify-content: center;
+      width: 4rem;
     }
   }
 }
